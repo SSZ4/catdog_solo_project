40a675f6ff3be39e22df32ba4cfab10781be354a
YangHun <pitahano@gmail.com>
2018-08-31 21:24:42 +0900

    Finish Main scene art design - add skybox - add some low poly mesh assets - change UI sprites - adjust title boxes; increase number, add scale resizing function, add glow

--- catdog solo/Assets/Color Rule/Color.cs
+++ catdog solo/Assets/Color Rule/Color.cs
@@ -2,137 +2,145 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Color : MonoBehaviour {
-	private float i = 0.0f;
-	// Use this for initialization
-	void Start () {
-		transform.GetComponent<Renderer>().material.color = UnityEngine.Color.white;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	private void OnCollisionEnter(Collision other)
+namespace SZ
+{
+
+	public class Color : MonoBehaviour
 	{
-		if (i == 0.0f)
-		{			
-			if (other.transform.tag == "Plane") // 태그는 임시로 설정함.
+		private float i = 0.0f;
+		// Use this for initialization
+		void Start()
+		{
+			transform.GetComponent<Renderer>().material.color = UnityEngine.Color.white;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+		private void OnCollisionEnter(Collision other)
+		{
+			if (i == 0.0f)
 			{
-				float or = other.transform.GetComponent<Renderer>().material.color.r;
-				float og = other.transform.GetComponent<Renderer>().material.color.g;
-				float ob = other.transform.GetComponent<Renderer>().material.color.b;
+				if (other.transform.tag == "Plane") // 태그는 임시로 설정함.
+				{
+					float or = other.transform.GetComponent<Renderer>().material.color.r;
+					float og = other.transform.GetComponent<Renderer>().material.color.g;
+					float ob = other.transform.GetComponent<Renderer>().material.color.b;
 
-				TileBlack(or, og, ob);
+					TileBlack(or, og, ob);
 
-				ColorLerp(or, og, ob); // 색상 혼합 시작 ~ 완료
+					ColorLerp(or, og, ob); // 색상 혼합 시작 ~ 완료
 
-				is_Black(); // 색상 변환 이후 검은색이 되었는지 확인
-							//transform.GetComponent<Renderer>().material.color = new Vector4(other.transform.GetComponent<Renderer>().material.color.r, other.transform.GetComponent<Renderer>().material.color.g, other.transform.GetComponent<Renderer>().material.color.b, 1.0f);
-				other.transform.GetComponent<Renderer>().material.color = UnityEngine.Color.white;	//new UnityEngine.Color(1.0f, 1.0f, 0.0f, 1.0f); //부딪힌 물체 색상 하얀색으로
-				i = 1.0f;
+					is_Black(); // 색상 변환 이후 검은색이 되었는지 확인
+								//transform.GetComponent<Renderer>().material.color = new Vector4(other.transform.GetComponent<Renderer>().material.color.r, other.transform.GetComponent<Renderer>().material.color.g, other.transform.GetComponent<Renderer>().material.color.b, 1.0f);
+					other.transform.GetComponent<Renderer>().material.color = UnityEngine.Color.white;  //new UnityEngine.Color(1.0f, 1.0f, 0.0f, 1.0f); //부딪힌 물체 색상 하얀색으로
+					i = 1.0f;
+				}
+
 			}
-			
 		}
-	}
-	private void OnCollisionExit(Collision collision)
-	{
-		i = 0.0f;
-	}
-
-	void ColorLerp(float or, float og, float ob)
-	{
-		float r = transform.GetComponent<Renderer>().material.color.r;
-		float g = transform.GetComponent<Renderer>().material.color.g;
-		float b = transform.GetComponent<Renderer>().material.color.b;
-
-		float k = RtoK(r, g, b);
-		float ok = RtoK(or, og, ob);
-
-		Debug.Log(k + " " + ok);
-
-		float c = RtoC(k, r);
-		float oc = RtoC(ok, or);
-
-		float m = RtoM(k, g);
-		float om = RtoM(ok, og);
-		
-		float y = RtoY(k, b);
-		float oy = RtoY(ok, ob);
-
-		float nk = k+ok - k*ok; //감산혼합 공식을 몰라서 임의로 대충 때려박음
-		float nc = c+oc - c*oc;
-		float nm = m+om - m*om;
-		float ny = y+oy - y*oy;
-
-		transform.GetComponent<Renderer>().material.color = new UnityEngine.Color(CtoR(nk, nc), CtoG(nk, nm), CtoB(nk, ny), 1.0f); // 색상 변환 완료
-	}
-
-	float RtoK(float r, float g, float b) // K값
-	{
-		float tmp = 0.0f;
-		if (r >= g && r >= b)
-			tmp = r;
-		else if (g >= b && g >= r)
-			tmp = g;
-		else
-			tmp = b;
-		
-		return 1 - tmp;
-	}
-
-	float RtoC(float k, float r) {
-		return (1 - k - r) / (1 - k);
-	}
-
-	float RtoM(float k, float g)
-	{
-		return (1 - k - g) / (1 - k);
-	}
-
-	float RtoY(float k, float b)
-	{		
-		return (1 - k - b) / (1 - k);
-	}
-
-
-	float CtoR(float nk, float nc)
-	{
-		return (1 - nc) * (1 - nk);
-	}
-
-	float CtoG(float nk, float nm)
-	{
-		return (1 - nm) * (1 - nk);
-	}
-
-	float CtoB(float nk, float ny)
-	{
-		return (1 - ny) * (1 - nk);
-	}
-
-	void TileBlack(float or, float ob, float og)
-	{
-		if(or == 0 && og == 0 && ob == 0)
+		private void OnCollisionExit(Collision collision)
 		{
-			Destroy(this.gameObject);
+			i = 0.0f;
 		}
-	}
 
-	void is_Black() //검은색인지 확인
-	{
-		float r = transform.GetComponent<Renderer>().material.color.r;
-		float g = transform.GetComponent<Renderer>().material.color.g;
-		float b = transform.GetComponent<Renderer>().material.color.b;
-
-		
-		if (r == 0 && g == 0 && b == 0)
+		void ColorLerp(float or, float og, float ob)
 		{
-			Destroy(this.gameObject);
+			float r = transform.GetComponent<Renderer>().material.color.r;
+			float g = transform.GetComponent<Renderer>().material.color.g;
+			float b = transform.GetComponent<Renderer>().material.color.b;
+
+			float k = RtoK(r, g, b);
+			float ok = RtoK(or, og, ob);
+
+			Debug.Log(k + " " + ok);
+
+			float c = RtoC(k, r);
+			float oc = RtoC(ok, or);
+
+			float m = RtoM(k, g);
+			float om = RtoM(ok, og);
+
+			float y = RtoY(k, b);
+			float oy = RtoY(ok, ob);
+
+			float nk = k + ok - k * ok; //감산혼합 공식을 몰라서 임의로 대충 때려박음
+			float nc = c + oc - c * oc;
+			float nm = m + om - m * om;
+			float ny = y + oy - y * oy;
+
+			transform.GetComponent<Renderer>().material.color = new UnityEngine.Color(CtoR(nk, nc), CtoG(nk, nm), CtoB(nk, ny), 1.0f); // 색상 변환 완료
 		}
-		if(transform.GetComponent<Renderer>().material.color == UnityEngine.Color.black)
+
+		float RtoK(float r, float g, float b) // K값
 		{
-			Destroy(this.gameObject);
+			float tmp = 0.0f;
+			if (r >= g && r >= b)
+				tmp = r;
+			else if (g >= b && g >= r)
+				tmp = g;
+			else
+				tmp = b;
+
+			return 1 - tmp;
+		}
+
+		float RtoC(float k, float r)
+		{
+			return (1 - k - r) / (1 - k);
+		}
+
+		float RtoM(float k, float g)
+		{
+			return (1 - k - g) / (1 - k);
+		}
+
+		float RtoY(float k, float b)
+		{
+			return (1 - k - b) / (1 - k);
+		}
+
+
+		float CtoR(float nk, float nc)
+		{
+			return (1 - nc) * (1 - nk);
+		}
+
+		float CtoG(float nk, float nm)
+		{
+			return (1 - nm) * (1 - nk);
+		}
+
+		float CtoB(float nk, float ny)
+		{
+			return (1 - ny) * (1 - nk);
+		}
+
+		void TileBlack(float or, float ob, float og)
+		{
+			if (or == 0 && og == 0 && ob == 0)
+			{
+				Destroy(this.gameObject);
+			}
+		}
+
+		void is_Black() //검은색인지 확인
+		{
+			float r = transform.GetComponent<Renderer>().material.color.r;
+			float g = transform.GetComponent<Renderer>().material.color.g;
+			float b = transform.GetComponent<Renderer>().material.color.b;
+
+
+			if (r == 0 && g == 0 && b == 0)
+			{
+				Destroy(this.gameObject);
+			}
+			if (transform.GetComponent<Renderer>().material.color == UnityEngine.Color.black)
+			{
+				Destroy(this.gameObject);
+			}
 		}
 	}
 }
